f93c87a029527f7c41e2ad63e5c22cf50a028f03
kadiatou barry <113573299+kadylius@users.noreply.github.com>
2024-05-07 08:59:24 -0700

    up

--- Objects_scripts/label_maker.js
+++ Objects_scripts/label_maker.js
@@ -4,6 +4,7 @@ let myObject ={
     firstname: "kadi",
     lastname: "Barry",
     age: "27",
+    emailadress: "kbarry@appdev.yearup.org",
     Githuburl: "https://github.com/kadylius"
 }
 console.log(myObject.age)
